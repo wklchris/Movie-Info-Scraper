d9b5bc1567aeea62a4da4f55d8ee347ae5204c9d
wklchris <wklchris@hotmail.com>
2024-12-20 00:10:05 -0800

    Fix tvtitle with Regex of webpage title.

--- MovieInfoScraper.user.js
+++ MovieInfoScraper.user.js
@@ -1,23 +1,25 @@
 // ==UserScript==
 // @name         Movie Info Scraper
 // @namespace    http://tampermonkey.net/
-// @version      0.2.2
+// @version      0.2.3
 // @description  Scrape the movie/TV information from a webpage.
 // @author       wklchris
-// @match      https://www.themoviedb.org/tv/*
+// @match        https://www.themoviedb.org/tv/*
 // @supportURL   https://github.com/wklchris/Movie-Info-Scraper
 // @grant        none
+// @downloadURL  https://update.greasyfork.org/scripts/408000/Movie%20Info%20Scraper.user.js
+// @updateURL    https://update.greasyfork.org/scripts/408000/Movie%20Info%20Scraper.meta.js
 // ==/UserScript==
 
 (function() {
   'use strict';
 
   // Check if HTML display language is ZH
-  var htmlLanguage = document.getElementsByTagName('html')[0].getAttribute('lang');
-  var isHtmlLangZh = (htmlLanguage.indexOf('zh') >= 0);
+  let htmlLanguage = document.getElementsByTagName('html')[0].getAttribute('lang');
+  let isHtmlLangZh = (htmlLanguage.indexOf('zh') >= 0);
 
-  var btnText = "Copy TV names to Clipboard";
-  var copyMessage = "TV filenames have been copied to clipboard. Example:";
+  let btnText = "Copy TV names to Clipboard";
+  let copyMessage = "TV filenames have been copied to clipboard. Example:";
   if (isHtmlLangZh) {
       btnText = "复制 TV 名称";
       copyMessage = "TV 名称已复制到剪贴板。示例：";
@@ -26,7 +28,7 @@
   // Copy a string variable to clipboard.
   function copyToClipboard(str, copyMessage) {
       // Create a textarea for copy
-      var strArea = document.createElement('textarea');
+      let strArea = document.createElement('textarea');
       strArea.value = str;
       document.body.appendChild(strArea);
       strArea.select();
@@ -43,10 +45,10 @@
       // Delete the element
       document.body.removeChild(strArea);
   }
-  
+
   // Replace invalid characters in filename strings
-  function escapeCharsInFilename(fname, isHtmlLangZh) {
-    var escapes = {
+  function escapeCharsInFilename(fname) {
+    let escapes = {
       "!": "！", "?": "？",
       ":": "：", ";": "；",
       "/": "-", "\\": "-", "|": "-",
@@ -59,7 +61,7 @@
         escapes[c] = ""
       }
     }
-    var s = fname;
+    let s = fname;
     for (const [c, val] of Object.entries(escapes)) {
       s = s.replaceAll(c, val);
     }
@@ -69,18 +71,19 @@
   // Copy all TV names in current webpage, with a Kodi-compatible format:
   //     [TV Series Name]-S[Season]E[Eposide].[Current Eposide Title]
   function copyTVNames(copyMessage='') {
-      var parentDOM = document.getElementsByTagName('h3');
+      let parentDOM = document.getElementsByTagName('h3');
       // Split by colon for tvtitle
-      var tvtitle = document.title.split(': ')[0].trim();
-      var fileNameGroup = [];
-      for (var i = 0; i < parentDOM.length; i++) {
-          var ep = parentDOM[i].querySelector(".no_click.open");
+      let title_regex = /^(.+?)(\(\d*\))?[\s—]*The Movie Database.*/g;
+      let tvtitle = document.title.replace(title_regex, '$1').replace(/\s+/g, '');
+      let fileNameGroup = [];
+      for (let i = 0; i < parentDOM.length; i++) {
+          let ep = parentDOM[i].querySelector(".no_click.open");
           if (ep != null) {
-              var season = ep.getAttribute('season');
-              var episode = ep.getAttribute('episode');
-              var eptitle = ep.innerHTML;
+              let season = ep.getAttribute('data-season-number');
+              let episode = ep.getAttribute('data-episode-number');
+              let eptitle = ep.innerHTML;
               // Filenames are in a Kodi-compatible format
-              var fname = tvtitle + '-S' + season.padStart(2, '0') + 'E' + episode.padStart(2, '0') + '.' + eptitle;
+              let fname = tvtitle + '-S' + season.padStart(2, '0') + 'E' + episode.padStart(2, '0') + '.' + eptitle;
               fname = escapeCharsInFilename(fname);
               fileNameGroup.push(fname);
           }
@@ -91,7 +94,7 @@
   }
 
   // Add a button for copyTVNames()
-  var btnMovieFileName = document.createElement('button');
+  let btnMovieFileName = document.createElement('button');
   btnMovieFileName.innerHTML = btnText;
   btnMovieFileName.onclick = function () { copyTVNames(copyMessage); };
   document.getElementsByClassName("filter")[0].appendChild(btnMovieFileName);
